6c38d5359b6f4ec83309e67cc07b93ddb827f641
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2025-10-25 22:54:01 +0200

    fix

--- print_tree.py
+++ print_tree.py
@@ -5,36 +5,30 @@ from colorama import init, Fore, Style
 init(autoreset=True)  # щоб кольори не "тягнулися" далі
 
 def print_tree(directory, indent=""):
-    """Рекурсивно виводить структуру папки."""
     for item in directory.iterdir():
         if item.is_dir():
-            # Папки сині
-            print(f"{indent}{Fore.BLUE}{item.name}/")
+            print(f"{indent}{Fore.BLUE}{item.name}/") # Папки сині
             print_tree(item, indent + "    ")
         else:
-            # Файли зелені
-            print(f"{indent}{Fore.GREEN}{item.name}")
+            print(f"{indent}{Fore.GREEN}{item.name}")  # Файли зелені
 
-def main():
-    # Отримуємо шлях із аргументів
-    if len(sys.argv) < 2:
-        print("Будь ласка, вкажіть шлях до папки.")
-        return
-
+if len(sys.argv) < 2:
+    print("Будь ласка, вкажіть шлях до папки.")
+else:
     path = Path(sys.argv[1])
 
-    # Перевірка на існування
     if not path.exists():
         print("Помилка: такий шлях не існує.")
-        return
-    if not path.is_dir():
+    elif not path.is_dir():
         print("Помилка: шлях не веде до директорії.")
-        return
+    else:
+        print(f"{Style.BRIGHT}Структура директорії: {path}\n")
+        print_tree(path)
 
-    print(f"{Style.BRIGHT}Структура директорії: {path}\n")
-    print_tree(path)
-
-if __name__ == "__main__":
-    main()
-
-    #python print_tree.py "/Users/helensolonikova/Desktop/Python/goit-pycore-hw-04"
+#python3 -m venv .venv
+#source .venv/bin/activate
+#pip install colorama
+#pip list
+#pip freeze > requirements.txt
+#pip install -r requirements.txt
+#python print_tree.py "/Users/helensolonikova/Desktop/Python/goit-pycore-hw-04"
